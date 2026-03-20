74d7164468749445ee3d33538ba2cbba108790c5
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2026-03-20 10:22:33 -0300

    terminado

--- pyLab/cuest3/ej7.py
+++ pyLab/cuest3/ej7.py
@@ -3,7 +3,20 @@ al usuario agregar tareas con descripción,
 fecha límite y prioridad, así como mostrar 
 la lista de tareas. 
 Este menú se repite hasta que el usuario elige salir."""
+salir=True
+tasks = []
 
-if __name__=="__main__":
-    name = input("Nombre de la tarea")
-    desc = input("Descripcion de")
+while salir:
+    name = input("Nombre de la tarea: ")
+    desc = input("Descripcion de la tarea: ")
+    dia = int(input("Dia de vencimiento: "))
+    mes = int(input("Mes de vencimiento: "))
+    prioridad = input("Urgente, Importante o normal? ")
+    task = [name,desc,dia,mes,prioridad]
+    tasks.append(task)
+    s = input("Salir o seguir? ").lower()
+    if s == "salir":
+        salir = False
+
+for task in tasks:
+    print(f"Prioridad: {task[4]}\nNombre: {task[0]}\nDescripcion: {task[1]}\nFecha de vencimiento: {dia}/{mes}")
